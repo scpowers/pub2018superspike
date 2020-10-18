9e8c512d4f29d34c54132fb67f38b1d26269dd30
Spencer Powers <spencer.powers197@gmail.com>
2020-10-18 00:13:32 -0700

    Updated run_randHeat.sh to use 5*51 neurons in the input and output layers, increased hidden layer neuron count to 500

--- sim/run_randHeat.sh
+++ sim/run_randHeat.sh
@@ -5,7 +5,7 @@ TMPDIR=`mktemp -d`
 trap "{ rm -rf $TMPDIR; }" EXIT
 
 EPSILON=1e-12
-NHID=256
+NHID=500
 RUN="randHeat"
 
 # for ETA in 1e-3 5e-4 1e-4 5e-3 ;
@@ -13,7 +13,7 @@ for ETA in 1e-3 ;
 do
 	OUTPUTDIR=../output/symfb/$RUN/eta$ETA
 	mkdir -p $OUTPUTDIR
-	make && ./sim_symfb --nin 51 --nout 51 --grid 20.099 --eta $ETA --input ../themes/${RUN}-input.ras --target ../themes/${RUN}-target.ras --simtime 200.99 --nhid $NHID --epsilon $EPSILON --delay 0.0e-3 --block 40 --w0 0.05 --layer 1 --dir $TMPDIR
+	make && ./sim_symfb --nin 255 --nout 255 --grid 20.099 --eta $ETA --input ../themes/${RUN}-input.ras --target ../themes/${RUN}-target.ras --simtime 200.99 --nhid $NHID --epsilon $EPSILON --delay 0.0e-3 --block 40 --w0 0.05 --layer 1 --dir $TMPDIR
 	cp $TMPDIR/*.stats $TMPDIR/*.spk $OUTPUTDIR
 done
 

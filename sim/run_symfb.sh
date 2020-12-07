d12b22c219a36ca5da22a472c91f875c2bedbb7e
Spencer <spencer.powers197@gmail.com>
2020-12-07 15:48:40 -0500

    Commit before switching machines

--- sim/run_symfb.sh
+++ sim/run_symfb.sh
@@ -16,9 +16,9 @@ echo HEIGHT=$HEIGHT
 # for ETA in 1e-3 5e-4 1e-4 5e-3 ;
 for ETA in 1e-3 ;
 do
-	OUTPUTDIR=../output/symfb/$RUN/eta$ETA
+	OUTPUTDIR=../output/symfb/$RUN/oxford_eta$ETA
 	mkdir -p $OUTPUTDIR
-	make && ./sim_symfb --nin 200 --nout $HEIGHT --grid $GRID --eta $ETA --input ../themes/${RUN}-input.ras --target ../themes/${RUN}-target.ras --simtime 20 --nhid $NHID --epsilon $EPSILON --delay 0.0e-3 --block 50 --w0 0.05 --layer 1 --dir $TMPDIR
+	make && ./sim_symfb --nin 200 --nout 200 --grid 1.89 --eta $ETA --input ../themes/${RUN}-input.ras --target ../themes/${RUN}-target.ras --simtime 20 --nhid $NHID --epsilon $EPSILON --delay 0.0e-3 --block 50 --w0 0.05 --layer 1 --dir $TMPDIR
 	cp $TMPDIR/*.stats $TMPDIR/*.spk $OUTPUTDIR
 done
 
